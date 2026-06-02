7d149495ab181451aade8dbb438681f6374435e8
matgomezc <maticogomez0@gmail.com>
2026-06-01 23:47:46 -0400

    actualizacion de pagina 2

--- src/componentes/CrudAdmin/PruebaCards.jsx
+++ src/componentes/CrudAdmin/PruebaCards.jsx
@@ -20,7 +20,7 @@ export const PruebaCards = () => {
           }}
         />
         <CardComponent
-          title="Revisar Ordenes de pene 🚚"
+          title="Revisar Ordenes de despacho 🚚"
           description="Consulta los despachos realizados, modifica los registros de intentos o cierra la orden"
           buttonText="Consultar"
           onClick={() => {
